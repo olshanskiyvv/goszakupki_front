b09f55b303e6b46e0ef77b42f08c48623e7f1589
viktory2023 <126407068+viktory2023@users.noreply.github.com>
2025-03-11 12:59:32 +0000

    new

--- src/pages/DocPages.jsx
+++ src/pages/DocPages.jsx
@@ -13,9 +13,11 @@ const Table = () => {
   return (
     <div clsddName="pageDoc">
     <h1>Законодательство в сфере закупок</h1>
+    <div className='bar'>
     <div className="search-bar">
       <input type="text" placeholder="Поиск по наименованию или содержанию документа" className="search-input" />
       <img src="/searchicon.svg" alt="Лупа" className="search-icon" />
+   </div>
    </div>
    <h2>Законодательство в сфере закупок</h2>
     <div className="table">
